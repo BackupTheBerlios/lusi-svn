b4acc582322c2e03d4d3a3609a1be25b34b7f2fc
kalvy <kalvy@bbff4365-c511-0410-b40e-d05a5ba0f6bf>
2006-10-14 13:00:05 +0000

    Added plural forms support to i18n.h Renamed macro parameters called "String" to "Msgid" in i18n.h (following GNU gettext.h example file)

--- trunk/lusi/src/lusi/util/i18n.h
+++ trunk/lusi/src/lusi/util/i18n.h
@@ -30,25 +30,33 @@
 #   include <libintl.h>
 
 /**
- * Translates the String to the current LUSI language.
- * The String is a const char*.
+ * Translates the Msgid to the current LUSI language.
+ * The Msgid is a const char*.
  * It uses dgettext instead of gettext, as the domain used must be the one of
  * the library, because a call simply to gettext may use the domain from the
  * application using the library.
  */
-#   define _(String) dgettext(PACKAGE, String)
+#   define _(Msgid) dgettext(PACKAGE, Msgid)
 
 /**
- * Marks the String to be translated.
- * Used when _(String) can't be used, for example, in array initializations.
+ * _(Msgid) equivalent using extended plural forms.
+ * First parameter is the singular form of the string, second parameter is the
+ * plural form, and third parameter is the number to check whether use singular,
+ * plural or a specific language form.
  */
-#   define gettext_noop(String) String
+#   define _n(Msgid1, Msgid2, N) dngettext(PACKAGE, Msgid1, Msgid2, N)
 
 /**
- * Short form of gettext_noop(String).
- * You must use it instead of gettext_noop(String).
+ * Marks the Msgid to be translated.
+ * Used when _(Msgid) can't be used, for example, in array initializations.
  */
-#   define N_(String) gettext_noop(String)
+#   define gettext_noop(Msgid) Msgid
+
+/**
+ * Short form of gettext_noop(Msgid).
+ * You must use it instead of gettext_noop(Msgid).
+ */
+#   define N_(Msgid) gettext_noop(Msgid)
 
 #else
 /* If NLS was disabled through the configure --disable-nls option, the macros
@@ -58,7 +66,9 @@
  * functions.
  */
 #   define _(Msgid) ((const char *) (Msgid))
-#   define N_(String) String
+#   define _n(Msgid1, Msgid2, N) \
+        ((N) == 1 ? (const char *) (Msgid1) : (const char *) (Msgid2))
+#   define N_(Msgid) Msgid
 
 #endif
 
